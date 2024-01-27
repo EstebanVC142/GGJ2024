3ae526dc256ea73b411a18caa09f802bb7e18ab1
Juan Pablo Correa Suarez <juanpabloc2000@gmail.com>
2024-01-27 01:33:36 -0500

    pad look and mouse look unified

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -35,22 +35,14 @@ public class Movement : MonoBehaviour
     {
         Vector2 movementAxis = input.actions["Move"].ReadValue<Vector2>();
         Vector2 lookAxis = input.actions["Look"].ReadValue<Vector2>();
-        if (input.actions["Look"].ReadValue<Vector2>().x != 0)
-            lookAxis = input.actions["Look"].ReadValue<Vector2>();
-        else if (input.actions["LookMouse"].ReadValue<Vector2>().x != 0)
-        {
-            lookAxis = input.actions["LookMouse"].ReadValue<Vector2>();
-            lookAxis.Normalize();
-        }
 
         rb.velocity = transform.forward * movementAxis.y * velocity;
         if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), groundDistance, layer))
         {
-            Debug.Log("funciona!");
-            rb.velocity -= new Vector3(0, gravity * Time.deltaTime, 0);
+            rb.velocity -= new Vector3(0, gravity * Time.fixedDeltaTime, 0);
         }
         else
             rb.velocity -= new Vector3(0, 0, 0);
-        rb.rotation *= Quaternion.Euler(transform.rotation.x, lookAxis.x * lookSensibility, transform.rotation.z);
+        rb.rotation *= Quaternion.Euler(transform.rotation.x, lookAxis.x * lookSensibility * Time.fixedDeltaTime, transform.rotation.z);
     }
 }
