f27bbea01e04c5053f52c6a0272bcdd849090332
Juan Pablo Correa Suarez <juanpabloc2000@gmail.com>
2024-01-27 00:59:56 -0500

    Movement improved and finished

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraPosition;
+    [SerializeField]
+    private float followTime = 1f;
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraPosition.position, followTime * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, cameraPosition.rotation, followTime * Time.deltaTime);
+    }
+}
