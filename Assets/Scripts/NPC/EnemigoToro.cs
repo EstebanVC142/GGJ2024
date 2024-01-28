fe2c259d2192bcf5597f2aed6ccafe3d6717f423
Esteban Vásquez Cano <estebanvasquezc20010106@gmail.com>
2024-01-28 02:25:02 -0500

    Override Enemigo Toro

---
+++ Assets/Scripts/NPC/EnemigoToro.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoToro : EstadosAnimal
+{
+
+    public override void EstadoAtacar()
+    {
+        base.EstadoAtacar();
+    }
+
+    public override void EstadoIdle()
+    {
+        base.EstadoIdle();
+    }
+
+    public override void EstadoMuerto()
+    {
+        base.EstadoMuerto();
+    }
+    public override void EstadoSeguir()
+    {
+        base.EstadoSeguir();
+    }
+}
